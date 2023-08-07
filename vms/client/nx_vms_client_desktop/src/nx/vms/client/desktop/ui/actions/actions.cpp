bd75628ac358351971c0ecf6ceab8da99ba2eb49
elrinor <ru.elric@gmail.com>
2023-08-07 14:05:45 +0800

    Added OpenReviewLayoutAction

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/actions/actions.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/actions/actions.cpp
@@ -2268,6 +2268,7 @@ void initialize(Manager* manager, Action* root)
 
     vx::registerDebugVxAction(factory);
     vx::registerDismissSystemAlertAction(factory);
+    vx::registerOpenReviewLayoutAction(factory);
 
     // -- Developer mode actions end. Please do not add real actions afterwards.
 }
