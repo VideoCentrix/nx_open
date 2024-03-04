91603dcbb3646c4e196d5d3d3a53b3147767ee0b
elrinor <ru.elric@gmail.com>
2024-03-04 22:32:43 +0000

    Open report layout for location action

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/menu/actions.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/menu/actions.cpp
@@ -1233,7 +1233,8 @@ void initialize(Manager* manager, Action* root)
         .text(ContextMenu::tr("Open Containing Folder"))
         .condition(new OpenInFolderCondition());
 
-    vx::registerOpenReportLayoutForResourceAction(factory);
+    vx::registerOpenReportLayoutForCameraAction(factory);
+    vx::registerOpenReportLayoutForLocationAction(factory);
 
     factory(IdentifyVideoWallAction)
         .flags(Tree | Scene | SingleTarget | MultiTarget | ResourceTarget | VideoWallItemTarget)
