0af8d205e2362b4355aa6e33d5b7db3c9eeeca53
elrinor <ru.elric@gmail.com>
2024-03-04 22:05:30 +0000

    Open report layout for resource action

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/actions/actions.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/actions/actions.cpp
@@ -1117,6 +1117,8 @@ void initialize(Manager* manager, Action* root)
         .text(ContextMenu::tr("Open Containing Folder"))
         .condition(new OpenInFolderCondition());
 
+    vx::registerOpenReportLayoutForResourceAction(factory);
+
     factory(IdentifyVideoWallAction)
         .flags(Tree | Scene | SingleTarget | MultiTarget | ResourceTarget | VideoWallItemTarget)
         .text(ContextMenu::tr("Identify"))
