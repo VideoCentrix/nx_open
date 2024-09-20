99726d10d3aeceb5799624a93a8d73dd9a87c929
Pavel Gurevich <pavelgur@pm.me>
2024-09-20 13:53:43 +0400

    added vxCallback within application_context

--- vms/libs/nx_vms_common/src/nx/vms/common/application_context.cpp
+++ vms/libs/nx_vms_common/src/nx/vms/common/application_context.cpp
@@ -85,6 +85,7 @@ ApplicationContext::~ApplicationContext()
     d->longRunableCleanup.reset();
     d->longRunnablePool.reset();
     deinitNetworking();
+    _vxCallback->deinit();
 
     if (NX_ASSERT(s_instance == this))
         s_instance = nullptr;
