12c625dbc4ea3a92a33a37800fca4a71e490f22e
Sergei Ivanov <sivanov@networkoptix.com>
2024-09-23 10:15:53 +0000

    VMS-55287, VMS-47799: Add Audio items playback indication, add "Mute" button
    Implement client features related to audio playback:
    * Fixes VMS-55287: Add "Mute" button on scene items when "Play all audio" mode is active
    * Fixes VMS-47799: Indicate when sound is played on item on scene

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/settings/system_specific_local_settings.h
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/settings/system_specific_local_settings.h
@@ -3,11 +3,13 @@
 #pragma once
 
 #include <map>
+#include <set>
 
 #include <QtCore/QString>
 
 #include <nx/utils/property_storage/storage.h>
 #include <nx/vms/client/desktop/system_context_aware.h>
+#include <nx/utils/uuid.h>
 
 namespace nx::vms::client::desktop {
 
@@ -22,6 +24,14 @@ class SystemSpecificLocalSettings:
 
 public:
     SystemSpecificLocalSettings(SystemContext* systemContext);
+
+    /** List of muted layout item ids.
+     *
+     * Note that the fact that we're only saving item ids here means that we need to GC them
+     * somehow. We do this on save just by checking the ids that we have agains all items in
+     * the resource pool. This works because even live viewers have access to layout resources
+     * for all other users (even though they don't see them in the UI). */
+    Property<std::set<QnUuid>> mutedItemIds{this, "mutedItemIds"};
 };
 
 } // namespace nx::vms::client::desktop
