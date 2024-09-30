53c7a1b78998982e3169a97dddfb6c3eba1f78fa
elrinor <ru.elric@gmail.com>
2024-09-30 15:48:33 +0100

    Mute / unmute button for local files

--- vms/client/nx_vms_client_desktop/src/ui/graphics/items/resource/media_resource_widget.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/graphics/items/resource/media_resource_widget.cpp
@@ -3531,12 +3531,22 @@ void QnMediaResourceWidget::at_triggerFieldsChanged(
     }
 }
 
+bool QnMediaResourceWidget::hasAudio() const
+{
+    if (d->camera)
+        return d->camera->isAudioEnabled();
+
+    if (d->mediaResource) // Handle local files.
+        return !d->mediaResource->getAudioLayout(d->display()->dataProvider())->tracks().empty();
+
+    return false;
+}
+
 bool QnMediaResourceWidget::canBeMuted() const
 {
     return
         ini().perItemMute &&
-        d->camera &&
-        d->camera->isAudioEnabled() &&
+        hasAudio() &&
         !isZoomWindow() &&
         qnSettings->playAudioForAllItems();
 }
@@ -3575,7 +3585,7 @@ void QnMediaResourceWidget::updateAudioPlaybackState()
 
     bool effectiveMuted =
         !isActiveWindow ||
-        (d->camera && !d->camera->isAudioEnabled()) ||
+        !hasAudio() ||
         (isPlayingAll ? canBeMuted() && isMuted() : !isCentral);
 
     if (shouldShowAudioSpectrum())
