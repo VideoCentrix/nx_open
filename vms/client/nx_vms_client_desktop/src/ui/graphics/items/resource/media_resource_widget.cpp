5e1dfb052956ee22c19ec9446366acff26fa83db
elrinor <ru.elric@gmail.com>
2024-10-03 18:26:04 +0100

    Take isAudioSupported into account in audio overlays

--- vms/client/nx_vms_client_desktop/src/ui/graphics/items/resource/media_resource_widget.cpp
+++ vms/client/nx_vms_client_desktop/src/ui/graphics/items/resource/media_resource_widget.cpp
@@ -810,7 +810,7 @@ bool QnMediaResourceWidget::mightShowAudioSpectrum() const
 
 bool QnMediaResourceWidget::shouldShowAudioSpectrum() const
 {
-    return mightShowAudioSpectrum() && d->camera && d->camera->isAudioEnabled();
+    return mightShowAudioSpectrum() && hasAudio();
 }
 
 QnMediaResourceWidget::AreaType QnMediaResourceWidget::areaSelectionType() const
@@ -3534,8 +3534,11 @@ void QnMediaResourceWidget::at_triggerFieldsChanged(
 
 bool QnMediaResourceWidget::hasAudio() const
 {
+    // We're checking for both isAudioSupported AND isAudioEnabled here because it's technically
+    // possible to have a camera that has audio enabled but not supported. Enabled is just a flag
+    // in the database.
     if (d->camera)
-        return d->camera->isAudioEnabled();
+        return d->camera->isAudioSupported() && d->camera->isAudioEnabled();
 
     if (d->mediaResource) // Handle local files.
         return !d->mediaResource->getAudioLayout(d->display()->dataProvider())->tracks().empty();
