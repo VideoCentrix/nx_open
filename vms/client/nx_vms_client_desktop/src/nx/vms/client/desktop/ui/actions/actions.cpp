edf4264b2ad38181a4a1943645232abfd0cf178e
Pavel Gurevich <pavelgur@pm.me>
2024-10-10 14:17:12 +0400

    visibility fix

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/actions/actions.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/actions/actions.cpp
@@ -667,7 +667,6 @@ void initialize(Manager* manager, Action* root)
 
     factory()
         .flags(Main)
-        .mode(DesktopMode)
         .text(ContextMenu::tr("Video Centrix"));
 
     factory.beginSubMenu();
