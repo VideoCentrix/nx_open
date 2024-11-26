cfb248a65c6ebbb869f7ec962285ccb55320a104
Pavel Gurevich <pavelgur@pm.me>
2024-11-26 13:17:48 +0400

    removed VxLocationPropertiesAction

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/actions/actions.h
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/actions/actions.h
@@ -1113,10 +1113,7 @@ enum IDType {
      */
     VxDismissSystemAlertAction,
 
-    /**
-     * Opens location properties dialog.
-     */
-    VxLocationPropertiesAction,
+    VxLocationPropertiesAction, // UNUSED
 
     /**
      * Sets spacing of current layout's cells to None.
