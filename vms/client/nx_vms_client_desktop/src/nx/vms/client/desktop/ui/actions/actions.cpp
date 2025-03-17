ae612211152eb4670c1df9042f08c3c682cf4772
elrinor <ru.elric@gmail.com>
2025-03-17 19:37:10 +0000

    Dropped in-client report editing

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/actions/actions.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/actions/actions.cpp
@@ -677,7 +677,6 @@ void initialize(Manager* manager, Action* root)
     factory.beginSubMenu();
     {
         vx::registerVxGlobalSystemHealth(factory);
-        vx::registerVxReportTemplates(factory);
     }
     factory.endSubMenu();
 
--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/actions/actions.h
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/actions/actions.h
@@ -1351,8 +1351,6 @@ enum IDType {
 
     VxGlobalSystemHealth,
 
-    VxReportTemplates,
-
     /**
      * Opens the Audit Log dialog.
      */
