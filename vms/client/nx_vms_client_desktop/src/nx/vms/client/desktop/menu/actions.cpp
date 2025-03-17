54983798e94b089b5ed4dcd0b27f0bad64cc803e
elrinor <ru.elric@gmail.com>
2025-03-17 19:37:10 +0000

    Dropped in-client report editing

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/menu/actions.cpp
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/menu/actions.cpp
@@ -746,7 +746,6 @@ void initialize(Manager* manager, Action* root)
     factory.beginSubMenu();
     {
         vx::registerVxGlobalSystemHealth(factory);
-        vx::registerVxReportTemplates(factory);
     }
     factory.endSubMenu();
 
