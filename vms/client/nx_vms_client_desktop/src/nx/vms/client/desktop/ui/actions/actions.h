73738207c04ff8fd3be8b819df1c680fff0d11e7
Pavel Gurevich <pavelgur@pm.me>
2025-05-01 10:55:52 +0400

    removed VxGlobalSystemHealth

--- vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/actions/actions.h
+++ vms/client/nx_vms_client_desktop/src/nx/vms/client/desktop/ui/actions/actions.h
@@ -1352,7 +1352,7 @@ enum IDType {
 
     VxGlobalDebugAction,
 
-    VxGlobalSystemHealth,
+    Vx_UNUSED,
 
     /**
      * Opens the Audit Log dialog.
@@ -1553,10 +1553,10 @@ enum IDType {
 
 Q_ENUM_NS(IDType)
 
-void initialize(Manager* manager, Action* root);
+void initialize(Manager *manager, Action *root);
 
 std::string toString(IDType id);
-bool fromString(const std::string_view& str, IDType* id);
+bool fromString(const std::string_view &str, IDType *id);
 
 NX_REFLECTION_TAG_TYPE(IDType, useStringConversionForSerialization)
 
